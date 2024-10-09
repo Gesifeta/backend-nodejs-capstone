79a1c236563844655d4d8ff798728092e43db1fe
Gemechu <95497037+Gesifeta@users.noreply.github.com>
2024-10-09 10:48:15 +0300

    Update server.js

--- secondChance-backend/server.js
+++ secondChance-backend/server.js
@@ -1,4 +1,4 @@
-/* jshint esversion: 8 */
+  /* jshint esversion: 8 */
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -12,7 +12,7 @@ const port = 3060
 
 //  Connect to MongoDB we just do this one time
 connectToDatabase().then(() => {
-pinoLogger.info('Connected to DB')
+  pinoLogger.info('Connected to DB')
 }).catch((e) => console.error('Failed to connect to DB', e))
 
 app.use(express.json())
@@ -29,13 +29,13 @@ app.use('/api/auth', userRoutes)
 app.use('/api/secondchance/items', secondChanceItemsRoutes)
 app.use('/api', searchRoutes)
 app.use((err, req, res, next) => {
-console.error(err)
-res.status(500).send('Internal Server Error')
+  console.error(err)
+  res.status(500).send('Internal Server Error')
 })
 
 app.get('/', (req, res) => {
-res.send('Inside the server')
+  res.send('Inside the server')
 })
 app.listen(port, () => {
-console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
